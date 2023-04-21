8d13e25b7a11dde11b0ec937a24722def480fc28
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2023-04-21 11:25:28 -0300

    Exemplo de iteração com map

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -2,32 +2,39 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-    
-    this.state = {
-      name : 'Emily Fitzlaff',
-      counter : 0
-    };
-  }
-
-  handleClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState( { name : 'Alterou o nome!' } );
-    this.setState( { counter : counter + 1 } );
-  }
+  state = {
+    posts : [
+      {
+        id: 1,
+        title: 'Título 1',
+        body: 'Body 1'
+      },
+      {
+        id: 2,
+        title: 'Título 2',
+        body: 'Body 2'
+      },
+      {
+        id: 3,
+        title: 'Título 3',
+        body: 'Body 3'
+      }      
+    ]
+  };
 
   render() {  
-    const { name, counter } = this.state;
-
+    const { posts } = this.state;
+   
     return (
       <div className="App">
-        <p onClick={this.handleClick}>Olá, { name } - { counter }</p>
+        {posts.map(post => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          )
+        )}
       </div>
-      
     )
   }
 }
